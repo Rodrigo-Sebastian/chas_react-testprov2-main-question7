30a9868d5d984e99ad2cd7d1529c8eecf8cfbaca
Rodrigo-Sebastian <rodrigo.seb93@gmail.com>
2024-06-02 16:52:44 +0200

    first commit

---
+++ app/pages/posts.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function Post() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((response) => response.json())
+        .then((data) => setPost(data));
+    }
+  }, [id]);
+
+  if (!post) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
